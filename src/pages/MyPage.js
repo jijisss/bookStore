92c83bac84f42a6527f7f6d2f3f743918d16de7c
jieun <sjy990302@naver.com>
2023-03-14 13:50:13 +0900

    Create MyPage.js

---
+++ src/pages/MyPage.js
@@ -0,0 +1,13 @@
+import "../components/top-btn.css";
+import { Link } from "react-router-dom";
+
+function MyPage() {
+  return (
+    <>
+      <h1>여기는</h1>
+      <h2>마이페이지</h2>
+    </>
+  );
+}
+
+export default MyPage;
